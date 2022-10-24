8245ecf09da92c1efcfd476409303136696172b0
Phuong Nguyen <dev@phuong.app>
2022-10-24 16:50:07 -0700

    Create firefox.py

---
+++ firefox.py
@@ -0,0 +1,7 @@
+from test import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+driver.get("https://www.selenium.dev/selenium/web/web-form.html")
